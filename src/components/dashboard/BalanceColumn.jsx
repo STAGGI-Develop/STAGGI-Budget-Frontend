7effde4e03f859e35e230095ce0a7c3d57ae127d
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-08-26 15:31:55 -0300

    feat (ui): Add responsive styles

--- src/components/dashboard/BalanceColumn.jsx
+++ src/components/dashboard/BalanceColumn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   Spinner,
   Stack,
@@ -10,103 +10,103 @@ import {
   Th,
   Thead,
   Tr,
-} from "@chakra-ui/react";
-import { apiTransaction } from "../../utils/apiCalls";
-import { useQuery } from "@tanstack/react-query";
+} from '@chakra-ui/react'
+import { apiTransaction } from '../../utils/apiCalls'
+import { useQuery } from '@tanstack/react-query'
 
 const BalanceCard = () => (
-  <Stack layerStyle="card">
+  <Stack layerStyle='card'>
     {/* Balance text */}
     <Stack
-      padding=".4rem"
-      direction="row"
-      justify="center"
-      align="center"
-      overflow="hidden"
-      alignSelf="stretch"
+      padding='.4rem'
+      direction='row'
+      justify='center'
+      align='center'
+      overflow='hidden'
+      alignSelf='stretch'
     >
       <Text
-        fontWeight="semibold"
-        fontSize="1.5rem"
-        color="gray.600"
-        textAlign="end"
+        fontWeight='semibold'
+        fontSize='1.5rem'
+        color='gray.600'
+        textAlign='end'
       >
         $
       </Text>
-      <Text fontWeight="semibold" fontSize="2.5rem" textAlign="end">
+      <Text fontWeight='semibold' fontSize='2.5rem' textAlign='end'>
         123.520
       </Text>
     </Stack>
 
     <Stack
-      direction="row"
-      justify="flex-start"
-      align="center"
-      spacing="1rem"
-      overflow="hidden"
-      flex="1"
-      alignSelf="stretch"
+      direction='row'
+      justify='flex-start'
+      align='center'
+      spacing='1rem'
+      overflow='hidden'
+      flex='1'
+      alignSelf='stretch'
     >
       {/* Income */}
       <Stack
-        justify="flex-start"
-        align="flex-start"
-        spacing=".25rem"
-        overflow="hidden"
-        flex="1"
-        alignSelf="stretch"
+        justify='flex-start'
+        align='flex-start'
+        spacing='.25rem'
+        overflow='hidden'
+        flex='1'
+        alignSelf='stretch'
       >
-        <Stack direction="row">
-          <Text fontWeight="medium" fontSize=".9rem" color="gray.500">
+        <Stack direction='row'>
+          <Text fontWeight='medium' fontSize='.9rem' color='gray.500'>
             Income
           </Text>
-          <Text fontWeight="bold" fontSize=".9rem" color="gray.700">
+          <Text fontWeight='bold' fontSize='.9rem' color='gray.700'>
             $25.240
           </Text>
         </Stack>
         <Stack
-          h="3rem"
-          padding=".5rem"
-          direction="row"
-          justify="center"
-          align="center"
-          alignSelf="stretch"
+          h='3rem'
+          padding='.5rem'
+          direction='row'
+          justify='center'
+          align='center'
+          alignSelf='stretch'
         >
-          <Stack w="full" h="2.5px" background="green" />
+          <Stack w='full' h='2.5px' background='green' />
         </Stack>
       </Stack>
 
       {/* Expending */}
       <Stack
-        justify="flex-start"
-        align="flex-start"
-        spacing=".25rem"
-        overflow="hidden"
-        flex="1"
-        alignSelf="stretch"
+        justify='flex-start'
+        align='flex-start'
+        spacing='.25rem'
+        overflow='hidden'
+        flex='1'
+        alignSelf='stretch'
       >
-        <Stack direction="row">
-          <Text fontWeight="medium" fontSize=".9rem" color="gray.500">
+        <Stack direction='row'>
+          <Text fontWeight='medium' fontSize='.9rem' color='gray.500'>
             Expending
           </Text>
-          <Text fontWeight="bold" fontSize=".9rem" color="gray.700">
+          <Text fontWeight='bold' fontSize='.9rem' color='gray.700'>
             $18.220
           </Text>
         </Stack>
         <Stack
-          h="3rem"
-          padding=".5rem"
-          direction="row"
-          justify="center"
-          align="center"
-          alignSelf="stretch"
+          h='3rem'
+          padding='.5rem'
+          direction='row'
+          justify='center'
+          align='center'
+          alignSelf='stretch'
         >
-          <Stack w="full" h="2.5px" background="staggi-pink.300" />
+          <Stack w='full' h='2.5px' background='staggi-pink.300' />
         </Stack>
       </Stack>
     </Stack>
   </Stack>
-);
+)
 
 const BalanceColumn = () => {
   const {
@@ -115,20 +115,20 @@ const BalanceColumn = () => {
     isError: isErrorTransactions,
     error: errorTransactions,
   } = useQuery({
-    queryKey: ["transactions"],
+    queryKey: ['transactions'],
     queryFn: apiTransaction.getAll,
-  });
+  })
   return (
     <Stack
       // w="full"
-      h="full"
-      maxH="full"
+      h='full'
+      maxH='full'
       flex={0.4}
-      direction="column"
-      spacing=".5rem"
+      direction='column'
+      spacing='.5rem'
     >
       <Stack>
-        <Text textStyle="cardHeader" color="gray.500">
+        <Text textStyle='cardHeader' color='gray.500'>
           Total balance
         </Text>
 
@@ -140,52 +140,52 @@ const BalanceColumn = () => {
        Pendiente botones
        --
        */}
-      <Stack h="full">
-        <Text marginTop="1rem" textStyle="cardHeader" color="gray.500">
+      <Stack h='full'>
+        <Text marginTop='1rem' textStyle='cardHeader' color='gray.500'>
           Last transactions
         </Text>
         {loadingTransactions ? (
           <Stack
-            w="full"
-            h="auto"
-            direction="column"
-            layerStyle="card"
-            spacing="0rem"
-            align="center"
+            w='full'
+            h='auto'
+            direction='column'
+            layerStyle='card'
+            spacing='0rem'
+            align='center'
           >
             <Spinner
-              margin="1.5rem"
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.100"
-              color="pink.500"
-              size="xl"
+              margin='1.5rem'
+              thickness='4px'
+              speed='0.65s'
+              emptyColor='gray.100'
+              color='pink.500'
+              size='xl'
             />
           </Stack>
         ) : (
-          <Stack w="full" h="full" layerStyle="card">
-            <TransactionsTable item={transactions?.data} />
+          <Stack w='full' h='full' layerStyle='card'>
+            <TransactionsTable item={transactions?.data} size='md' />
           </Stack>
         )}
       </Stack>
     </Stack>
-  );
-};
+  )
+}
 
-export default BalanceColumn;
+export default BalanceColumn
 
-const TransactionsTable = ({ item }) => {
+export const TransactionsTable = ({ item, size }) => {
   const CustomRow = ({ tr }) => (
     <Tr>
       <Td>{tr.Type}</Td>
       <Td>{tr.Description}</Td>
       <Td isNumeric>{`$${tr.Amount}`}</Td>
     </Tr>
-  );
+  )
   return (
     <TableContainer>
-      <Table size="md">
-        <Thead background="gray.100">
+      <Table size={size}>
+        <Thead background='gray.100'>
           <Tr>
             <Th>Icon</Th>
             <Th>Description</Th>
@@ -199,5 +199,5 @@ const TransactionsTable = ({ item }) => {
         </Tbody>
       </Table>
     </TableContainer>
-  );
-};
+  )
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { Stack } from "@chakra-ui/react";
-import BalanceColumn from "../components/dashboard/BalanceColumn";
-import SavingsBudgetsColumn from "../components/dashboard/SavingBudgetColumn";
-import ChartColumn from "../components/dashboard/ChartColumn";
+import { Stack } from '@chakra-ui/react'
+import BalanceColumn from '../components/dashboard/BalanceColumn'
+import SavingsBudgetsColumn from '../components/dashboard/SavingBudgetColumn'
+import ChartColumn from '../components/dashboard/ChartColumn'
 
 const Home = () => {
   return (
     <Stack
-      h="full"
-      marginX="10rem"
-      marginY="1.5rem"
-      direction="row"
-      spacing="2rem"
+      h='full'
+      marginX={{
+        base: '1rem',
+        sm: '4rem',
+        md: '10rem',
+        lg: '4rem',
+        xl: '10rem',
+      }}
+      marginY='1.5rem'
+      direction={{ base: 'column', lg: 'row' }}
+      spacing='2rem'
       // overflow="hidden"
       // justify="flex-start"
       // align="flex-start"
@@ -23,7 +28,7 @@ const Home = () => {
       {/* <Balance /> */}
       {/* <Chart /> */}
     </Stack>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
